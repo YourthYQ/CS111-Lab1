af01d51bb6f8c311191e6809b96ada500dafdef3
Yourth.YQ <sapphireyq@gmail.com>
2024-04-26 20:31:22 -0700

    New

---
+++ pipe.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <program1> [program2 ...]\n", argv[0]);
+        return EINVAL;  // Invalid argument
+    }
+
+    int num_commands = argc - 1;
+    int i;
+    int pipe_fds[2 * (num_commands - 1)];  // Array to hold the file descriptors for all pipes
+
+    // Create all necessary pipes
+    for (i = 0; i < num_commands - 1; i++) {
+        if (pipe(pipe_fds + i * 2) < 0) {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for (i = 0; i < num_commands; i++) {
+        pid_t pid = fork();
+        if (pid == 0) {  // Child process
+            // If not the first command, get input from the previous pipe
+            if (i > 0) {
+                if (dup2(pipe_fds[(i - 1) * 2], STDIN_FILENO) < 0) {
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            // If not the last command, output to the next pipe
+            if (i < num_commands - 1) {
+                if (dup2(pipe_fds[i * 2 + 1], STDOUT_FILENO) < 0) {
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            // Close all pipe file descriptors
+            for (int j = 0; j < 2 * (num_commands - 1); j++) {
+                close(pipe_fds[j]);
+            }
+
+            execlp(argv[i + 1], argv[i + 1], (char *)NULL);
+            perror("execlp");  // execlp only returns on error
+            exit(EXIT_FAILURE);
+        } else if (pid < 0) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Parent closes all pipe file descriptors
+    for (i = 0; i < 2 * (num_commands - 1); i++) {
+        close(pipe_fds[i]);
+    }
+
+    // Wait for all child processes to finish
+    for (i = 0; i < num_commands; i++) {
+        wait(NULL);
+    }
+
+    return 0;
+}
