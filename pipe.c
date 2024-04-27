a1aa418a0918dc81cd823cff97ef595bf6d3c368
Yourth.YQ <sapphireyq@gmail.com>
2024-04-26 21:53:37 -0700

    Update pipe.c

--- pipe.c
+++ pipe.c
@@ -6,9 +6,11 @@
 #include <errno.h>
 
 int main(int argc, char *argv[]) {
+
+	// Check if No argument at the command line
     if (argc < 2) {
         fprintf(stderr, "Usage: %s [program1] [program2]...\n", argv[0]);
-        return EINVAL;  // Invalid argument
+        return EINVAL; // Invalid argument
     }
 
     int num_commands = argc - 1;
@@ -27,20 +29,22 @@ int main(int argc, char *argv[]) {
 
     int status;
     for (i = 0; i < num_commands; i++) {
+
         pid_t pid = fork();
         if (pid == 0) {  // Child process
-            // If not the first command, get input from the previous pipe
+
+            // If not the first command, redirect stdin from the read end of the previous pipe
             if (i > 0) {
                 if (dup2(pipe_fds[(i - 1) * 2], STDIN_FILENO) < 0) {
-                    perror("dup2");
+                    perror("Failed to set up stdin");
                     exit(EXIT_FAILURE);
                 }
             }
 
-            // If not the last command, output to the next pipe
+            // If not the last command, redirect stdout to the write end of the current pipe
             if (i < num_commands - 1) {
                 if (dup2(pipe_fds[i * 2 + 1], STDOUT_FILENO) < 0) {
-                    perror("dup2");
+                    perror("Failed to set up stdout");
                     exit(EXIT_FAILURE);
                 }
             }
@@ -50,16 +54,20 @@ int main(int argc, char *argv[]) {
                 close(pipe_fds[j]);
             }
 
+			// Execute the command using execlp: if execlp fails, output error message and exit
             execlp(argv[i + 1], argv[i + 1], (char *)NULL);
-            perror("Wrong argument: ");  // execlp only returns on error
-            exit(errno);  // Exit with the errno set by execlp
+            perror("Wrong argument");
+            exit(errno); // Exit with the errno set by execlp
+
         } else if (pid < 0) {
-            perror("fork");
+			// Output an error message and exit if process creation (fork) fails
+            perror("Failed to create a new process");
             exit(EXIT_FAILURE);
         }
+
     }
 
-    // Parent closes all pipe file descriptors
+    // Parent process closes all pipe file descriptors after forking
     for (i = 0; i < 2 * (num_commands - 1); i++) {
         close(pipe_fds[i]);
     }
@@ -68,6 +76,7 @@ int main(int argc, char *argv[]) {
     int exit_status = 0;
     for (i = 0; i < num_commands; i++) {
         wait(&status);
+		// Check if any child process exited with a status other than 0 (indicating an error)
         if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
             exit_status = WEXITSTATUS(status);
         }
