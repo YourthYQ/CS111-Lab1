69a1d859007df749d75db88476148dd3952f0ebb
Yourth.YQ <sapphireyq@gmail.com>
2024-04-26 21:19:44 -0700

    Update pipe.c

--- pipe.c
+++ pipe.c
@@ -6,27 +6,34 @@
 #include <errno.h>
 
 int main(int argc, char *argv[]) {
+
+	// No argument in the command line
     if (argc < 2) {
-        perror("No argument");
-		exit(EINVAL);
+        fprintf(stderr, "Usage: %s [program1] [program2]...\n", argv[0]);
+        return EINVAL;
     }
 
-    int num_commands = argc - 1;
+    int num_of_commands = argc - 1;
     int i;
-    int pipe_fds[2 * (num_commands - 1)];  // Array to hold the file descriptors for all pipes
+
+	// Create an array to hold the file descriptors for all pipes
+    int pipe_fds[2 * (num_commands - 1)];
 
     // Create all necessary pipes
     for (i = 0; i < num_commands - 1; i++) {
         if (pipe(pipe_fds + i * 2) < 0) {
-            perror("pipe");
+            perror("Pipe");
             exit(EXIT_FAILURE);
         }
     }
 
-    int status;
     for (i = 0; i < num_commands; i++) {
+
+		// Create a new process
         pid_t pid = fork();
+
         if (pid == 0) {  // Child process
+
             // If not the first command, get input from the previous pipe
             if (i > 0) {
                 if (dup2(pipe_fds[(i - 1) * 2], STDIN_FILENO) < 0) {
@@ -43,18 +50,19 @@ int main(int argc, char *argv[]) {
                 }
             }
 
-            // Close all pipe file descriptors in the child process
+            // Close all pipe file descriptors
             for (int j = 0; j < 2 * (num_commands - 1); j++) {
                 close(pipe_fds[j]);
             }
 
             execlp(argv[i + 1], argv[i + 1], (char *)NULL);
-            perror("execlp");  // execlp only returns on error
-            exit(errno);  // Exit with the errno set by execlp
+            perror("Wrong argument: ");  // execlp only returns on error
+            exit(EXIT_FAILURE);
         } else if (pid < 0) {
             perror("fork");
             exit(EXIT_FAILURE);
         }
+		
     }
 
     // Parent closes all pipe file descriptors
@@ -62,14 +70,10 @@ int main(int argc, char *argv[]) {
         close(pipe_fds[i]);
     }
 
-    // Parent waits for all child processes to finish and checks for any errors
-    int exit_status = 0;
+    // Wait for all child processes to finish
     for (i = 0; i < num_commands; i++) {
-        wait(&status);
-        if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
-            exit_status = WEXITSTATUS(status);
-        }
+        wait(NULL);
     }
 
-    return exit_status;
+    return 0;
 }
